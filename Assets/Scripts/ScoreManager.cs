94a45393b33976d3581f44381904b20ef7e7e296
NicoChiprut <nico.chiprut@gmail.com>
2025-05-30 10:38:06 -0300

    Clase 30/5

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public float money;
+
+    public void UpdateMoney(float amount)
+    {
+        if (money + amount < 0)
+        {
+            //Impedir la compra
+            Debug.Log("Dinero Insufixiente");
+        }
+        else
+        {
+            money += amount;
+        }
+
+    }
+
+}
